814776f3b0b2a484033a43cc3e76a5019bc38b5c
Overloord53 <bniangboss@gmail.com>
2023-01-23 10:19:22 +0100

    sd

--- Assets/Scripts/RandomMobSpawner.cs
+++ Assets/Scripts/RandomMobSpawner.cs
@@ -7,17 +7,21 @@ public class RandomMobSpawner : MonoBehaviour
     public Transform[] spawnpoints;
     public GameObject[] ememyPrefabs;
     public static bool spawnAllowed;
+    public int spawnLimit = 5;
+    public int spawned;
     
     void Start()
     {
+        spawned = 0;
         spawnAllowed = true;
-        InvokeRepeating("SpawnMobs", 2f, 3f );
+        InvokeRepeating("SpawnMobs", 2f, 30f );
     }
 
     void SpawnMobs()
     {
         if (spawnAllowed)
         {
+            
             int randEnemyindex = Random.Range(0, ememyPrefabs.Length);
             int randSpawPoint = Random.Range(0, spawnpoints.Length);
             
--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -7,17 +7,23 @@ public class RandomSpawner : MonoBehaviour
     public Transform[] spawnpoints;
     public GameObject[] ememyPrefabs;
     public static bool spawnAllowed;
+    public int spawnLimit = 5;
+    public int spawned;
     
     void Start()
     {
         spawnAllowed = true;
-        InvokeRepeating("SpawnMobs", 8f, 4f );
+        InvokeRepeating("SpawnMobs", 8f, 10f );
     }
 
     void SpawnMobs()
     {
         if (spawnAllowed)
         {
+            if (spawned <= spawnLimit)
+            {
+                spawned++;
+            }
             int randEnemyindex = Random.Range(0, ememyPrefabs.Length);
             int randSpawPoint = Random.Range(0, spawnpoints.Length);
             
