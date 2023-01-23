6e4b62def83a16792bd52ecec1fa7f83e9764812
Overloord53 <bniangboss@gmail.com>
2023-01-23 12:20:33 +0100

    hg

--- Assets/Scripts/RandomMobSpawner.cs
+++ Assets/Scripts/RandomMobSpawner.cs
@@ -6,15 +6,16 @@ public class RandomMobSpawner : MonoBehaviour
 {
     public Transform[] spawnpoints;
     public GameObject[] ememyPrefabs;
+    
     public static bool spawnAllowed;
-    public int spawnLimit = 5;
-    public int spawned;
+    
     
     void Start()
     {
-        spawned = 0;
+        
         spawnAllowed = true;
-        InvokeRepeating("SpawnMobs", 2f, 30f );
+        InvokeRepeating("SpawnMobs", 2f, 3f );
+      
     }
 
     void SpawnMobs()
@@ -27,7 +28,8 @@ public class RandomMobSpawner : MonoBehaviour
             
 
             Instantiate(ememyPrefabs[randEnemyindex], spawnpoints[randSpawPoint].position, transform.rotation);
-
+            
+            
 
 
         }
--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -1,39 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class RandomSpawner : MonoBehaviour
 {
     public Transform[] spawnpoints;
     public GameObject[] ememyPrefabs;
-    public static bool spawnAllowed;
-    public int spawnLimit = 5;
-    public int spawned;
+
+    public float EnemyInterval = 3f;
+    public int maxEnemies = 3;
+    private int currentEnemies = 0;
+    
     
     void Start()
     {
-        spawnAllowed = true;
-        InvokeRepeating("SpawnMobs", 8f, 10f );
+        StartCoroutine(SpawnEnemys());
     }
 
-    void SpawnMobs()
+    private IEnumerator SpawnEnemys()
     {
-        if (spawnAllowed)
+        while (true)
         {
-            if (spawned <= spawnLimit)
+            if (currentEnemies < maxEnemies)
             {
-                spawned++;
-            }
-            int randEnemyindex = Random.Range(0, ememyPrefabs.Length);
-            int randSpawPoint = Random.Range(0, spawnpoints.Length);
+                
+                yield return new WaitForSeconds(GetInterval());
             
-
-            Instantiate(ememyPrefabs[randEnemyindex], spawnpoints[randSpawPoint].position, transform.rotation);
-
-
-
+                int randSpawPoint = Random.Range(0, spawnpoints.Length);
+                int randEnemyindex = Random.Range(0, ememyPrefabs.Length);
+                Instantiate(ememyPrefabs[randEnemyindex], spawnpoints[randSpawPoint].position, transform.rotation);
+                
+            }
         }
     }
-    
+
+    public float GetInterval()
+    {
+        if (ScoreScript.scoreValue >= 100)
+        {
+            return 1;
+        }
+
+        return EnemyInterval;
+    }
+
+   
+
     
 }
